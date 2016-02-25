262d9c98a26d0234e355eb3bddc50d10b77cf001
Will Morgan <wmorgan@factual.com>
2016-02-24 20:21:56 -0800

    Rendering state

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,21 +1,46 @@
 (ns tictactoe.core
-  (:require [compojure.core :refer :all]
+  (:require [clojure.string :as string]
+            [compojure.core :refer :all]
             [compojure.route :as route]))
+
+(def game-state
+  [[" ", "X", " "]
+   [" ", "O", "X"]
+   ["O", " ", " "]])
+
+(defn board-as-string
+  [board]
+  (string/join "-----\n"
+               (for [row board]
+                 (str (string/join "|" row) "\n"))))
 
 (defn show-board
   [req]
   (def r req)
   (str "<pre>"
-       " | |X\n"
-       "-----\n"
-       " |O| \n"
-       "-----\n"
-       " | |X\n"
+       (board-as-string game-state)
        "</pre>"))
 
 (defroutes handler
   (GET "/" [] "<h1>Hello World</h1>")
-  (GET "/foo" [] "<h1>Hello Foo</h1>")
-  (GET "/bar" [] "<h1>Hello Bar</h1>")
   (GET "/board" [] show-board)
   (route/not-found "<h1>Page not found</h1>"))
+
+
+
+
+
+
+
+
+
+
+(assert
+  (= (board-as-string [[" ", "X", " "]
+                       [" ", " ", "X"]
+                       [" ", " ", " "]])
+     (str " |X| \n"
+          "-----\n"
+          " | |X\n"
+          "-----\n"
+          " | | \n")))
